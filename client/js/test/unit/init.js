4d339f8e575ecd72850605551f8de95edb84fb2e
Michael <mbenin.cn@gmail.com>
2014-10-08 14:55:03 -0400

    Issue #2 - webdriver fixed

--- client/js/test/unit/init.js
+++ client/js/test/unit/init.js
@@ -1,13 +1,3 @@
 'use strict';
 
-var expect = require('chai').expect;
-
-describe('Testing tests.', function () {
-
-  it('will equal equal, I HOPE!', function () {
-    expect('equal').to.equal('equal');
-  });
-
-});
-
 require('../core/state_manager');
